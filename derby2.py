79ea2cf1e72904d254ec080068d89843f03c625b
Carl Hester <crucialcarl@gmail.com>
2016-01-18 16:39:18 -0500

    no idea

--- derby2.py
+++ derby2.py
@@ -10,6 +10,10 @@ RND = Randomizer.Randomizer()
 
 execfile("seed.py")
 
+import Outputter
+OUT = Outputter.Outputter()
+
+OUT.writeToTemplate(0, "test", "output")
 # admin interface
 
 # presentation
